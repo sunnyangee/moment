11dcc8f8f2f538732336efe3b72e88d0e0b86f02
sunnyangee <106881815+sunnyangee@users.noreply.github.com>
2025-04-25 07:08:00 +0900

    Add enabled field to User entity

--- src/test/java/com/example/moment/MomentApplicationTests.java
+++ src/test/java/com/example/moment/MomentApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-  // 테스트 시에는 H2 메모리 DB를 쓰도록 오버라이드
+  // 테스트 시점에 H2 메모리 DB를 사용
   "spring.datasource.url=jdbc:h2:mem:testdb",
   "spring.datasource.driver-class-name=org.h2.Driver",
   "spring.datasource.username=sa",
@@ -14,7 +14,6 @@ class MomentApplicationTests {
 
   @Test
   void contextLoads() {
-    // 빈 로딩만 확인
+    // 단순히 컨텍스트 로딩만 검사
   }
-
 }
