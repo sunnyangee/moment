100073a77a57e1d3349c239b99e7d2388cc0d7fd
sunnyangee <106881815+sunnyangee@users.noreply.github.com>
2025-04-25 06:59:13 +0900

    Add enabled field to User entity

--- src/main/java/com/example/moment/MomentApplication.java
+++ src/main/java/com/example/moment/MomentApplication.java
@@ -2,9 +2,13 @@ package com.example.moment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.moment.config.PasswordProperties;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(PasswordProperties.class)
 public class MomentApplication {
 
 	public static void main(String[] args) {
