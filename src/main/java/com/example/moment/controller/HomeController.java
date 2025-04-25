7ee82e91837c82a5379be9fceada79ce8e591947
sunnyangee <106881815+sunnyangee@users.noreply.github.com>
2025-04-26 04:13:21 +0900

    Add enabled field to User entity

--- src/main/java/com/example/moment/controller/HomeController.java
+++ src/main/java/com/example/moment/controller/HomeController.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class HomeController {
 
     private static final List<String> SEQUENCE = List.of(
-        "start", "3-2", "3-3", "homeroom", "cafeteria", "nurse-office", "principalsoffice", "1ftoilet", "audiovisualroom", "overnightroom", "counselingroom", "1felevator", "1fstairs", "library", "teachersroom", "computerroom", "clubroom1", "clubroom2", "clubroom3", "artroom", "broadcastingroom", "studentcouncilroom", "bridge", "gym", "gym2f", "clubroom4", "clubroom5", "clubroom6", "stairs2f", "musicroom", "conveniencestore", "clubroom7", "clubroom8", "englishroom", "supplyroom", "scienceroom", "powersupplyroom", "computerserverroom", "pharmacy", "convenience", "atm"
+        "start", "3-2", "3-3", "homeroom", "cafeteria", "nurse-office", "principalsoffice", "1ftoilet", "audiovisualroom", "overnightroom", "counselingroom", "1felevator", "1fstairs", "library", "teachersroom", "computerroom", "clubroom1", "clubroom2", "clubroom3", "artroom", "broadcastingroom", "studentcouncilroom", "bridge", "gym", "gym2f", "clubroom4", "clubroom5", "clubroom6", "stairs2f", "musicroom", "conveniencestore", "clubroom7", "clubroom8", "englishroom", "supplyroom", "scienceroom", "powersupplyroom", "computerserverroom", "pharmacy", "convenience", "atm", "foodcourt", "cookingroom"
     );
 
     private final ProgressService progressService;
@@ -234,6 +234,16 @@ public class HomeController {
     @GetMapping("/atm") // ← 하이픈 없이
     public String atm(@AuthenticationPrincipal UserPrincipal userPrincipal, Model model) {
         return handleStage("atm", userPrincipal, model);
+    }
+
+    @GetMapping("/foodcourt") // ← 하이픈 없이
+    public String foodcourt(@AuthenticationPrincipal UserPrincipal userPrincipal, Model model) {
+        return handleStage("foodcourt", userPrincipal, model);
+    }
+
+    @GetMapping("/cookingroom") // ← 하이픈 없이
+    public String cookingroom(@AuthenticationPrincipal UserPrincipal userPrincipal, Model model) {
+        return handleStage("cookingroom", userPrincipal, model);
     }
 
 
